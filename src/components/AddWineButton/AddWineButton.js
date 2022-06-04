deeacf71b008c85ebab92e89abb9de4fbf0b2415
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-04 15:54:08 +0200

    Add styles to AddWineButton

--- src/components/AddWineButton/AddWineButton.js
+++ src/components/AddWineButton/AddWineButton.js
@@ -3,7 +3,8 @@ import styles from './AddWineButton.module.scss';
 const AddWineButton = () => {
     return (
         <div className={styles.addWineButton}>
-            <a href="/" ><span className='fa fa-plus'></span>Dodaj wino</a>
+            <a href="/" >Dodaj wino</a>
+            <span className='fa fa-plus'></span>
         </div>
         
     )
