dbab119e7afe3d30d96859e5c329140f4385ca5f
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-15 18:34:40 +0200

    Test FilterList component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import ButtonAddWine from '../ButtonAddWine/ButtonAddWine';
 import ButtonShowWineList from '../ButtonShowWineList/ButtonShowWineList';
+import FiltersList from '../FiltersList/FiltersList';
 
 const Home = () => {
     return (
         <main>
             <ButtonAddWine />
             <ButtonShowWineList />
+            <FiltersList />
         </main>    
     )
 }
--- src/components/ShopsList/ShopsList.js
+++ src/components/ShopsList/ShopsList.js
@@ -1,4 +1,4 @@
-import ShopCard from "../ShopCard/ShopCard";
+import FilterCard from '../FilterCard/FilterCard';
 import styles from './ShopsList.module.scss';
 import { useSelector } from 'react-redux';
 import shortid from 'shortid';
@@ -7,12 +7,13 @@ const ShopsList = () => {
     const getAllShops = ({ wines }) => wines.map(wine => wine.shop);
     const allShops = useSelector(state => getAllShops(state));
     const shops = [...new Set(allShops)];
+    console.log(shops);
 
     return (
         <section>
         <h2>Gdzie jesteś?</h2>
         <ul className={styles.shopsList}>
-          {shops.map(shop => <ShopCard key={shortid()} >{shop}</ ShopCard>)}
+          {shops.map(shop => <FilterCard key={shortid()} >{shop}</ FilterCard>)}
         </ul>
 
 
