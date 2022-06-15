b1709eb9a464430f2269fcc65524a923a3261299
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-15 17:28:46 +0200

    Clear initialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,39 +1,5 @@
 const initialState = {
-    wines: [
-        {
-            name: 'Rosario',
-            grapeVariety: 'Shiraz',
-            country: 'Portugal',
-            shop: 'Lidl'
-        },
-        {
-            name: 'Madame Bobalu',
-            grapeVariety: 'Bobalu',
-            country: 'Spain',
-            shop: 'Kondrat Wina Wybrane',
-        },
-        {
-            name: 'Fauno',
-            grapeVariety: 'Primitivo',
-            country: 'Italy',
-            shop: 'Lidl',
-        },
-        {
-            name: "Nero d'oro",
-            grapeVariety: "Nero d'Avola",
-            country: 'Italy',
-            shop: 'Mila',
-        },
-        {
-            name: "Mezzek",
-            grapeVariety: "Mavrud",
-            country: 'Bulgaria',
-            shop: 'Żabka',
-        },
-
-    ],
-    
-
+    wines: [],
 }
 
 export default initialState;
