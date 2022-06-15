8b362b371aca83c645a5428e68e7a0e8df671cc7
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-15 18:33:01 +0200

    Add FilterList component

---
+++ src/components/FiltersList/FiltersList.js
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux';
+import FilterCard from '../FilterCard/FilterCard';
+import shortid from 'shortid';
+
+const FiltersList = () => {
+
+    const getAllCategories = ({ wines }) => wines.map(wine => Object.keys(wine));
+    const allCategories = useSelector(state => getAllCategories(state));
+    const categories = [...new Set(allCategories.flat(1))];
+    const correctCategories = categories.filter(category => category !== 'name');
+
+    return (
+        <section>
+        <h2>Search your wines by:</h2>
+        <ul>
+          {correctCategories.map(category => <FilterCard key={shortid()} category={category} >{category}</ FilterCard>)}
+        </ul>
+
+
+        </section>
+        
+    )
+}
+
+export default FiltersList;
