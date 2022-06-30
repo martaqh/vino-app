0b9aefea27f58256726e1381f0f7cfc5d9514c8a
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-06-30 18:27:00 +0200

    Extract WineInfoDisplay component

--- src/components/WineCard/WineCard.js
+++ src/components/WineCard/WineCard.js
@@ -1,42 +1,22 @@
 import styles from './WineCard.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGlobe, faLeaf, faShoppingBasket, faTint } from '@fortawesome/free-solid-svg-icons';
-import { ImageUploader } from '../ImageUploader/ImageUploader';
-import ButtonRemoveWine from '../ButtonRemoveWine/ButtonRemoveWine';
 import Accordion from '../Accordion/Accordion';
-
+import { useState } from 'react';
+import WineInfoDisplay from '../WineInfoDisplay/WineInfoDisplay';
 
 const WineCard = props => {
-
+    const [isEdited, setIsEdited] = useState(false);
+    if (isEdited === false)
     return (
-            <Accordion title={props.name}
-                       content={ <div className={styles.wineCard}>
-                       <section>
-                           <FontAwesomeIcon icon={faTint} />
-                           <p>{props.color}</p>
-                       </section>
-                       <section>
-                           <FontAwesomeIcon icon={faLeaf} />
-                           <p>{props.grapes}</p>
-                       </section>
-                       
-                       <section className={styles.wineCard__country}>
-                           <FontAwesomeIcon icon={faGlobe} />
-                           <p>{props.country}</p>
-                       </section>
-           
-                       <section>
-                           <FontAwesomeIcon icon={faShoppingBasket} />
-                           <p>{props.shop}</p>
-                       </section>
-                       
-                       <ImageUploader />
-                       <ButtonRemoveWine wineId={props.id} />
-                       
-                   </div>
-
-                       } />
+            <Accordion {...props} className={styles.wineCard}
+                       title={props.name}
+                       content={<WineInfoDisplay {...props} toggleEdit={setIsEdited} />} />
         
+    )
+    return (
+        <Accordion className={styles.wineCard}
+                       title={props.name}
+                       content={<div>Edit me</div>}
+                        />
     )
 }
 
---
+++ src/components/WineInfoDisplay/WineInfoDisplay.js
@@ -0,0 +1,40 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGlobe, faLeaf, faShoppingBasket, faTint } from '@fortawesome/free-solid-svg-icons';
+import { ImageUploader } from '../ImageUploader/ImageUploader';
+import ButtonRemoveWine from '../ButtonRemoveWine/ButtonRemoveWine';
+import styles from './WineInfoDisplay.module.scss';
+
+const WineInfoDisplay = props => {
+    console.log(props);
+    return (
+        <div className={styles.wineCard} >
+                       <section>
+                           <FontAwesomeIcon icon={faTint} />
+                           <p>{props.color}</p>
+                       </section>
+                       <section>
+                           <FontAwesomeIcon icon={faLeaf} />
+                           <p>{props.grapes}</p>
+                       </section>
+                       
+                       <section>
+                           <FontAwesomeIcon icon={faGlobe} />
+                           <p>{props.country}</p>
+                       </section>
+           
+                       <section>
+                           <FontAwesomeIcon icon={faShoppingBasket} />
+                           <p>{props.shop}</p>
+                       </section>
+                       
+                       <ImageUploader />
+                       <ButtonRemoveWine wineId={props.id} />
+                       <button onClick={props.toggleEdit}>Edit</button>
+                       
+                       
+                   </div>
+    )
+}
+
+export default WineInfoDisplay;
+
