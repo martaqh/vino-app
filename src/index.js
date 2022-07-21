3638d7129dcd1c2e98524d841dba0dd89af5a0d4
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-21 22:18:33 +0200

    Switch BrowserRouter and Provider in index.js

--- src/index.js
+++ src/index.js
@@ -9,14 +9,17 @@ import 'font-awesome/css/font-awesome.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      
       <Provider store={store}>
+      <BrowserRouter>
       
         <App />
+
+        </BrowserRouter>
         
       </Provider>
 
-      </BrowserRouter>
+     
     
     
   </React.StrictMode>
