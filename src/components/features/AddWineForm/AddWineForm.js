a7bf34b662310af8ed393b4702bb55e5c158fa93
Marta Kurian-Herma <marta.k.herma@gmail.com>
2022-07-25 12:43:07 +0200

    Restore original version of capitalize function

--- src/components/features/AddWineForm/AddWineForm.js
+++ src/components/features/AddWineForm/AddWineForm.js
@@ -26,12 +26,14 @@ const AddWineForm = props => {
 
     const navigate = useNavigate();
 
-    const capitalize = input => {
-        const words = input.split(" ");
+    const capitalize = str => {
+       /* const words = str.split(" ");
         for (let i = 0; i < words.length; i++) {
             words[i] = words[i][0].toUpperCase() + words[i].substr(1);
         }
-        return words.join(" ");    
+        
+        return words.join(" "); */
+        return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     }
 
     const handleSubmit = e => {
